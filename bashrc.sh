fbeb42592e29319e34a7d303d63b1b1772629250
chirsz-ever <chirsz@foxmail.com>
2021-04-01 18:34:52 +0800

    increase readability

--- bashrc.sh
+++ bashrc.sh
@@ -9,11 +9,22 @@ alias ls='ls --color=auto'
 alias grep='grep --color=auto'
 alias la='ls -A'
 alias ll='ls -alF'
-#PS1='\n\[\033[32m\]\u\[\033[0m\]@\h \[\e[34m\]\w\[\033[0m\]\n\[\033[33m\]\$\[\033[0m\] '
-if [ "$UID" -eq 0 ]; then
-	export PS1='\n\[\e[38;5;196m\]\u\[\e[0m\]@\[\e[38;5;166m\]\h\[\e[0m\]:\[\e[38;5;118m\]\w\[\e[0m\]\n\$ '
-else
-	export PS1='\n\[\e[38;5;135m\]\u\[\e[0m\]@\[\e[38;5;166m\]\h\[\e[0m\]:\[\e[38;5;118m\]\w\[\e[0m\]\n\$ '
-fi
-export PS1="\[\e]0;\u@\h:\w\a\]$PS1"
 
+ps1_machine='\[\e[38;5;166m\]\h\[\e[0m\]'
+ps1_pwd='\[\e[38;5;118m\]\w\[\e[0m\]'
+
+if [ "$UID" -eq 0 ]; then
+	ps1_user='\[\e[38;5;196m\]\u\[\e[0m\]'
+else
+	ps1_user='\[\e[38;5;135m\]\u\[\e[0m\]'
+fi
+export PS1="$ps1_user@$ps1_machine:$ps1_pwd\n\\$ "
+
+# set xterm title
+if [ $TERM = xterm* ]; then
+    export PS1="\[\e]0;\u@\h:\w\a\]$PS1"
+fi
+
+unset ps1_user
+unset ps1_machine
+unset ps1_pwd
